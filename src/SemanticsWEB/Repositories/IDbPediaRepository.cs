8263fa9227292a92c7912bf082249fd710f3a539
bpark <burt.parkers@googlemail.com>
2018-12-28 11:42:59 +0100

    added doc

--- src/SemanticsWEB/Repositories/IDbPediaRepository.cs
+++ src/SemanticsWEB/Repositories/IDbPediaRepository.cs
@@ -1,8 +1,10 @@
 namespace SemanticsWEB.Repositories
 {
+    /// <summary>
+    /// Repository to access dbpedia.
+    /// </summary>
     public interface IDbPediaRepository
     {
-
         void doIt();
     }
 }
--- src/SemanticsWEB/Repositories/ISesameRepository.cs
+++ src/SemanticsWEB/Repositories/ISesameRepository.cs
@@ -3,6 +3,9 @@ using SemanticsWEB.Models;
 
 namespace SemanticsWEB.Repositories
 {
+    /// <summary>
+    /// Repository to access sesame stores.
+    /// </summary>
     public interface ISesameRepository
     {
         IEnumerable<Triple> Query();
