3d68090f4f70a39d4e1b642006a7c73897117044
bpark <burt.parkers@gmail.com>
2019-01-12 16:10:24 +0100

    added ttl data

---
+++ configs/import.sh
@@ -0,0 +1,3 @@
+curl -X POST -H  "Content-Type: application/x-www-form-urlencoded" -d "type=native&Repository+ID=currencies&Repository+title=tr+currencies&Triple+indexes=spoc%2Cposc&EvaluationStrategyFactory=org.eclipse.rdf4j.query.algebra.evaluation.impl.StrictEvaluationStrategyFactory" "http://192.168.33.10:8080/rdf4j-workbench/repositories/NONE/create"
+
+curl -X POST --data-binary @data/currency.ttl.gz -H "Content-Encoding: gzip" -H "Content-Type: text/turtle" http://192.168.33.10:8080/rdf4j-server/repositories/currencies/statements
